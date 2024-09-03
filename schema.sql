c19854995ab2372a15d07af48c25ac2023286df7
Julian Bolanos <julian.camilob24@gmail.com>
2024-09-03 18:09:34 -0400

    feat (inserts): Added test inserts and modified tables schema to sequential ids

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
 -- Create the Chats table
 CREATE TABLE Chats (
-    chat_id UUID PRIMARY KEY,
+    chat_id SERIAL PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
-    patient_id UUID NOT NULL,
+    patient_id INT NOT NULL,
     chat_reason VARCHAR(255),
     feedback_score INT CHECK(feedback_score BETWEEN 0 AND 10)
 );
 
 -- Create the Appointments table
 CREATE TABLE Appointments (
-    appointment_id UUID PRIMARY KEY,
+    appointment_id SERIAL PRIMARY KEY,
     appointment_created_at TIMESTAMP NOT NULL,
     appointment_starts_at TIMESTAMP NOT NULL,
-    chat_id UUID REFERENCES Chats(chat_id),
-    patient_id UUID NOT NULL
+    chat_id INT REFERENCES Chats(chat_id),
+    patient_id INT NOT NULL
 );
