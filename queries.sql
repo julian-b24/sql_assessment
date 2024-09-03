49bcc64daf42aeb1e31697fa763138194eab8487
Julian Bolanos <julian.camilob24@gmail.com>
2024-09-03 19:08:03 -0400

    fix (query): Fixed Order of difference operation in query to request avg time of scheduling since the chat is created

--- queries.sql
+++ queries.sql
@@ -5,6 +5,6 @@ GROUP BY c.chat_reason,  TO_CHAR(a.appointment_starts_at, 'MM')
 ORDER BY TO_CHAR(a.appointment_starts_at, 'MM');
 
 
-SELECT avg(c.created_at - a.appointment_created_at) as "Promedio Tiempo Chat hasta Agendamiento"
+SELECT avg(a.appointment_created_at - c.created_at) as "Promedio Tiempo Chat hasta Agendamiento"
 FROM Chats c, Appointments a
 WHERE c.chat_id = a.chat_id;
