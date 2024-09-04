7b15bfb0da8bb3ed2b447cc969963abed6b19de7
Julian Bolanos <julian.camilob24@gmail.com>
2024-09-03 22:44:43 -0400

    fix (query): Fixed queries that requiered patient id

--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
 SELECT TO_CHAR(a.appointment_starts_at, 'MM') as "Mes", c.chat_reason as "Motivo de Chat", count(*) as "Numero de Citas"
 FROM Chats c, Appointments a
-WHERE c.chat_id = a.chat_id -- AND c.patient_id = a.patient_id
+WHERE c.chat_id = a.chat_id AND c.patient_id = a.patient_id
 GROUP BY c.chat_reason,  TO_CHAR(a.appointment_starts_at, 'MM')
 ORDER BY TO_CHAR(a.appointment_starts_at, 'MM');
 
 
 SELECT avg(a.appointment_created_at - c.created_at) as "Promedio Tiempo Chat hasta Agendamiento"
 FROM Chats c, Appointments a
-WHERE c.chat_id = a.chat_id; -- AND c.patient_id = a.patient_id
+WHERE c.chat_id = a.chat_id AND c.patient_id = a.patient_id;
 
 
 SELECT avg(a.appointment_starts_at - a.appointment_created_at) as "Anticipación Promedio"
