79361d3fe8e5e0f0338d1be1d68570447c1edc63
Julian Bolanos <julian.camilob24@gmail.com>
2024-09-03 21:23:34 -0400

    feat (query): Formating chat data for frequency histogram

--- queries.sql
+++ queries.sql
@@ -12,3 +12,43 @@ WHERE c.chat_id = a.chat_id;
 
 SELECT avg(a.appointment_starts_at - a.appointment_created_at) as "Anticipación Promedio"
 FROM Appointments a;
+
+WITH feedback_score_table as (
+    SELECT '30 días' as "Intervalo",
+            count(case when c.feedback_score >= 9 then 1 end) as "Promotores",
+            count(case when c.feedback_score = 7 OR c.feedback_score = 8 then 1 end) as "Neutral",
+            count(case when c.feedback_score <= 6 then 1 end) as "Detractores",
+            count(*) as "Total Puntuaciones"
+    FROM Chats c
+    WHERE c.created_at BETWEEN now() AND  now() - INTERVAL '30 DAY'
+    UNION
+    SELECT '3 mes' as "Intervalo",
+            count(case when c.feedback_score >= 9 then 1 end) as "Promotores",
+            count(case when c.feedback_score = 7 OR c.feedback_score = 8 then 1 end) as "Neutral",
+            count(case when c.feedback_score <= 6 then 1 end) as "Detractores",
+            count(*) as "Total Puntuaciones"
+    FROM Chats c
+    WHERE c.created_at BETWEEN now() AND  now() - INTERVAL '90 DAY'
+    UNION
+    SELECT '1 año' as "Intervalo",
+            count(case when c.feedback_score >= 9 then 1 end) as "Promotores",
+            count(case when c.feedback_score = 7 OR c.feedback_score = 8 then 1 end) as "Neutral",
+            count(case when c.feedback_score <= 6 then 1 end) as "Detractores",
+            count(*) as "Total Puntuaciones"
+    FROM Chats c
+    WHERE c.created_at BETWEEN now() AND  now() - INTERVAL '365 DAY'
+    UNION
+    SELECT 'Todo el tiempo' as "Intervalo",
+            count(case when c.feedback_score >= 9 then 1 end) as "Promotores",
+            count(case when c.feedback_score = 7 OR c.feedback_score = 8 then 1 end) as "Neutral",
+            count(case when c.feedback_score <= 6 then 1 end) as "Detractores",
+            count(*) as "Total Puntuaciones"
+    FROM Chats c
+)
+SELECT "Intervalo", (100 * ("Promotores")/"Total Puntuaciones") - (100 * "Detractores"/"Total Puntuaciones") as "NPS"
+FROM feedback_score_table f;
+
+WITH total_chats AS ( SELECT count(*) AS total FROM Chats)
+SELECT c.patient_id as "Paciente", count(*) as "Frecuencua", 100 * count(*)/t.total as "Proporción (%)"
+FROM Chats c, total_chats t
+GROUP BY c.patient_id, t.total;
